2d6fe21cde8dac74ca1973ddf5b4909ade877b35
nathansiva18 <nathansiva018@gmail.com>
2022-12-04 12:45:50 +0530

    kuttil first

---
+++ src/test/java/org/s/D.java
@@ -0,0 +1,17 @@
+package org.s;
+
+public class D {
+	private void jagadeesh() {
+		System.out.println("waran");
+	}
+	private void siva() {
+		System.out.println("nathan");
+	}
+	private void kuttil() {
+		System.out.println("monkey");
+	}
+	private void ramesh() {
+		System.out.println("khan");
+	}
+
+}
